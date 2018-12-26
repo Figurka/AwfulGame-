1928748aecf05688d3334d8ee5b3e3dc0a103796
nastygleb88 <glebka5103@gmail.com>
2018-12-26 21:48:33 +0300

    add func update for bullets'

--- method.cpp
+++ method.cpp
@@ -26,3 +26,71 @@ void Enemy::update(float time)
 {
 
 };
+
+//////////Bullet//////////
+Bullet::Bullet(Image &image, float X, float Y, int W, int H, std::string Name, int dir) :Smth(image, X, Y, W, H, Name)
+{
+	x = X; //координаты пули на карте игры
+	y = Y;
+	direction = dir; //направление полета пули 
+	Speed = 6;
+	w = W;
+	h = H;//размеры изображения пули 
+	Life = true; //пуля жива
+	sprite.setTextureRect(IntRect(88, 3, w, h));
+
+};
+
+
+
+
+void Bullet::update(float time)
+{
+
+	switch (direction)
+	{
+	case 0:
+	{
+		dx = -Speed;
+		dy = 0;
+		break;
+	} //     state = left 
+	case 1:
+	{
+		dx = Speed;
+		dy = 0;
+		break;
+	} //       state = right 
+	case 2:
+	{
+		dx = 0;
+		dy = -Speed;
+		break;
+	} //       state = up 
+	case 3:
+	{
+		dx = 0;
+		dy = Speed;
+		break;
+	}//       tate = down 
+	}
+	if (Life)
+	{ // если пуля жива 
+		x += dx * 0.1*time;//само движение пули по х
+		y += dy * 0.1*time;//по у
+		for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты 
+			for (int j = x / 32; j < (x + w) / 32; j++)
+			{
+				if (TileMap[i][j] == '1')
+				{
+					Life = false;// то пуля умирает 
+				}
+				sprite.setPosition(x + w, y + h);
+			}
+	}      //задается позицию пули 
+};
+
+void  Bullet::interactionWithMap(float Dx, float Dy)
+{
+	Life = false;
+};
